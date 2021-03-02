74f0af24d5c8392b1967710a24b56bb2bd27bbf4
Mich <michola3636@gmail.com>
2021-03-02 22:11:36 +0000

    16

--- server.js
+++ server.js
@@ -18,6 +18,20 @@ connectDB();
 const app = express()
 app.use(express.json())
 
+app.use(function(req,res, next){
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+
+
+
+})
+
+
+
+
+
 app.use("/api/products", productRoutes)
 
 const PORT = process.env.PORT || 5000
